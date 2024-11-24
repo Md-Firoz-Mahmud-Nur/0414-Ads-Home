e1cb48e846c535d57c7fbb0bc9a40fac1e635c2b
firoz.nur <firoznur5@gmail.com>
2024-11-24 10:15:58 +0600

    Change : Name Font Weight

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           <div className="flex gap-4 items-center justify-center">
             <img className="size-10" src={manPng} alt="" />
             <div className="flex flex-col gap-1 pb-1">
-              <h1 className="font-extrabold">Mahafuzur Rahman</h1>
+              <h1 className="font-bold">Mahafuzur Rahman</h1>
               <h1 className="rounded-full bg-white py-0.5  pl-2 text-black">
                 Tap to balance
               </h1>
