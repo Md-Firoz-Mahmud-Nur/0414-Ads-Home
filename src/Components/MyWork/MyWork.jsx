150e45764d1463fad651c8b3bf087a7cb8045934
firoz.nur <firoznur5@gmail.com>
2024-11-30 11:15:17 +0600

    Add : My Work Card

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,7 +1,82 @@
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
+
 const MyWork = () => {
   return (
-    <div>
-      this is my work
+    <div className="min-h-[calc(100vh-64px)] bg-[#EDEDF5]">
+      <div className="container mx-auto py-6 gap-6 flex flex-col">
+        {/* card 1 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+        {/* card 2 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+        {/* card 3 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+        {/* card 4 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+        {/* card 5 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+        {/* card 6 */}
+        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+          <div className="flex items-center justify-end gap-1">
+            100.00 <FaBangladeshiTakaSign />
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Work</div>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black">
+              Click Here
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        cardBackground: "url('/src/assets/work-bg-blue.png')",
+      },
+    },
   },
   plugins: [daisyui],
 };
