5e1adaa4d5ae7b63e83ae83570d854e50db00ce3
firoz.nur <firoznur5@gmail.com>
2024-12-01 13:17:39 +0600

    Change : Minor Text

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -3,74 +3,74 @@ import { FaBangladeshiTakaSign } from "react-icons/fa6";
 const MyWork = () => {
   return (
     <div className="min-h-[calc(100vh-64px)] bg-[#EDEDF5]">
-      <div className="container mx-auto py-6 gap-6 flex flex-col">
+      <div className="container mx-auto flex flex-col gap-6 px-1 py-6">
         {/* card 1 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
           </div>
         </div>
         {/* card 2 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
           </div>
         </div>
         {/* card 3 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
           </div>
         </div>
         {/* card 4 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
           </div>
         </div>
         {/* card 5 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
           </div>
         </div>
         {/* card 6 */}
-        <div className="bg-cardBackground flex h-full w-full flex-col gap-4 rounded-lg bg-cover bg-center p-6 text-white">
+        <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-cardBackground bg-cover bg-center p-6 text-white">
           <div className="flex items-center justify-end gap-1">
             100.00 <FaBangladeshiTakaSign />
           </div>
           <div className="flex items-center justify-between">
-            <div>Work</div>
+            <div>Survey</div>
             <button className="btn btn-outline text-white hover:bg-white hover:text-black">
               Click Here
             </button>
