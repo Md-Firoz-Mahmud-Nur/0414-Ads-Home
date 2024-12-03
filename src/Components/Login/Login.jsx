a4b2bce3364edb2ca88e2cb78c5ad4529eec0602
firoz.nur <firoznur5@gmail.com>
2024-12-03 13:50:56 +0600

    Add : Login Logo

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,14 @@
+import Logo from "../../assets/logo.png";
+
 const Login = () => {
-  return <div>this is login</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-64px)] items-center justify-center bg-[#EDEDF5]">
+      <div className="container mx-auto flex max-h-min flex-col items-center border bg-white p-4">
+        <img className="size-60" src={Logo} alt="" />
+        <div className="w-full border p-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
