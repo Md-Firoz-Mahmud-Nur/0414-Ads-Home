ae15c2a812f796b55d3707995740c0fdd4ada8f3
firoz.nur <firoznur5@gmail.com>
2024-12-05 17:49:14 +0600

    Add : Register Form

--- src/Components/Login/Register.jsx
+++ src/Components/Login/Register.jsx
@@ -5,7 +5,69 @@ const Register = () => {
     <div className="flex min-h-[calc(100vh-64px)] items-center justify-center bg-[#EDEDF5]">
       <div className="container mx-auto flex max-h-min flex-col items-center border bg-white p-4">
         <img className="size-60" src={Logo} alt="" />
-        <div className="w-full border p-4"></div>
+        {/* form */}
+        <form className="mt-10 flex w-10/12 flex-col gap-4">
+          <div className="form-control col-span-2 w-full md:col-span-1">
+            <label className="input-group">
+              <input
+                type="text"
+                min={0}
+                className="input input-bordered w-full placeholder:text-gray-500"
+                name="fullName"
+                placeholder="Enter Full Name"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control col-span-2 w-full md:col-span-1">
+            <label className="input-group">
+              <input
+                type="number"
+                min={0}
+                className="input input-bordered w-full placeholder:text-gray-500"
+                name="fullName"
+                placeholder="Enter Mobile Number"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control col-span-2 w-full md:col-span-1">
+            <label className="input-group">
+              <input
+                type="password"
+                min={0}
+                className="input input-bordered w-full placeholder:text-gray-500"
+                name="fullName"
+                placeholder="Enter Password"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control col-span-2 w-full md:col-span-1">
+            <label className="input-group">
+              <input
+                type="password"
+                min={0}
+                className="input input-bordered w-full placeholder:text-gray-500"
+                name="fullName"
+                placeholder="Re Enter Password"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-control col-span-2 w-full md:col-span-1">
+            <label className="input-group">
+              <input
+                type="text"
+                min={0}
+                className="input input-bordered w-full placeholder:text-gray-500"
+                name="fullName"
+                placeholder="Enter Refer"
+                required
+              />
+            </label>
+          </div>
+        </form>
       </div>
     </div>
   );
