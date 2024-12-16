a65ff0f622e92f6b0cdaec91f2221b0a38e879fe
firoz.nur <firoznur5@gmail.com>
2024-12-16 17:37:54 +0600

    Add : Logout From Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { TbLogout } from "react-icons/tb";
+import { TbLogin, TbLogout } from "react-icons/tb";
 import Sidebar from "./Sidebar/Sidebar";
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user, signOutUser, setUser } = useContext(AuthContext);
+  const signOut = () => {
+    signOutUser()
+      .then(() => {
+        setUser(null);
+      })
+      .catch(() => {});
+  };
+
   return (
     <div className="h-16 bg-blue-500">
       <div className="container mx-auto flex justify-between text-white">
@@ -10,7 +22,9 @@ const Navbar = () => {
           <div className="flex items-center justify-center gap-4">
             <Sidebar></Sidebar>
             <div className="flex flex-col gap-1 pb-1">
-              <h1 className="font-bold">Mahafuzur Rahman</h1>
+              <h1 className="font-bold">
+                {user?.email || "log in to see email"}
+              </h1>
               <h1 className="rounded-full bg-white py-0.5 pl-2 text-black">
                 Tap to balance
               </h1>
@@ -19,7 +33,15 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-3 pr-4">
           <IoMdNotificationsOutline className="size-7"></IoMdNotificationsOutline>
-          <TbLogout className="size-7"></TbLogout>
+          {user ? (
+            <Link onClick={signOut}>
+              <TbLogout className="size-7"></TbLogout>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <TbLogin className="size-7"></TbLogin>
+            </Link>
+          )}
         </div>
       </div>
     </div>
