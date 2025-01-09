2ad2ff1b2b639fab68c9a2098930e25b578e22e1
firoz.nur <firoznur5@gmail.com>
2025-01-09 21:23:09 +0600

    Add Design : My Profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -23,17 +23,63 @@ const Profile = () => {
 
   return (
     <div>
-      <p>Balance: {role?.balance}</p>
-      <p>your name: {role?.name}</p>
-      <p>your email: {role?.email}</p>
-      <p>your phone number: {role?.mobileNumber}</p>
-      <p>refer by: {role?.referBy}</p>
-      <p>Your refer Code: {role?.referCode}</p>
-      <p>role: {role?.role}</p>
-      <p>
-        Created at:{" "}
-        {role?.createdAt ? formatDate(role.createdAt) : "Loading..."}
-      </p>
+      <div className="min-h-[calc(100vh-64px)] bg-[#EDEDF5] px-2 pt-8">
+        <div className="px- container mx-auto gap-6 rounded-lg bg-white p-6 shadow-md">
+          <h2 className="mb-6 border-b pb-4 text-2xl font-bold text-blue-500">
+            Profile Details
+          </h2>
+          <div className="grid grid-cols-1 gap-6">
+            <div>
+              <h3 className="text-sm text-gray-500">Balance</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.balance} Taka
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Your Name</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.name}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Your Email</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.email}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Your Phone Number</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.mobileNumber}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Referred By</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.referBy || "Not Referred"}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Your Refer Code</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.referCode}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Role</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {role?.role}
+              </p>
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-500">Created At</h3>
+              <p className="text-lg font-semibold text-gray-700">
+                {formatDate(role?.createdAt)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
