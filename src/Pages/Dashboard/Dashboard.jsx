c39bc04948e2e0834ea1251f9b778a27ad2a7c23
firoz.nur <firoznur5@gmail.com>
2025-01-11 08:31:12 +0600

    Add : Link Route Button

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
   return (
-    <div>
-      this is a dashboard
+    <div className="flex min-h-[calc(100vh-64px)] items-center bg-[#EDEDF5] px-2 pt-8">
+      <div className="px- container mx-auto flex flex-col gap-6 rounded-lg bg-white p-6 shadow-md">
+        <Link to="/dashboard/addLink" className="btn btn-success text-white">
+          Add Link
+        </Link>
+        <Link className="btn btn-info text-white">View Links</Link>
+        <Link to="/" className="btn btn-error text-white">
+          Delete Link
+        </Link>
+      </div>
     </div>
   );
 };
