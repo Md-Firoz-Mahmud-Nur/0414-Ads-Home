502e0a3b9eed5f9a1403e67dd60431881f07cc04
firoz.nur <firoznur5@gmail.com>
2025-01-16 18:30:19 +0600

    Get : Added Links

--- src/Pages/Dashboard/ViewLinks.jsx
+++ src/Pages/Dashboard/ViewLinks.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+
 const ViewLinks = () => {
-  return (
-    <div>
-this is view links
-    </div>
-  );
+  const axiosPublic = useAxiosPublic();
+  const { data: viewLinks = "" } = useQuery({
+    queryKey: ["viewLinks"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("getAddedLinks");
+      return res.data;
+    },
+  });
+
+  console.log(viewLinks);
+
+  return <div>this is view links</div>;
 };
 
 export default ViewLinks;
